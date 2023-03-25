d519d5d812f549ff773449997b732a78ec957660
Michal Motyčka <mychalmotycka@gmail.com>
2023-03-25 20:43:35 +0100

    added badges removed unused method

--- src/Ridge/GeneralHelpers.cs
+++ src/Ridge/GeneralHelpers.cs
@@ -65,47 +65,6 @@ internal static class GeneralHelpers
         return Convert.GetTypeCode(type) != TypeCode.Object;
     }
 
-    public static IDictionary<string, string>? ToKeyValue(
-        object? metaToken)
-    {
-        if (metaToken == null)
-        {
-            return null;
-        }
-
-        var token = metaToken as JToken;
-        if (token == null)
-        {
-            return ToKeyValue(JObject.FromObject(metaToken));
-        }
-
-        if (token.HasValues)
-        {
-            var contentData = new Dictionary<string, string>();
-            foreach (var child in token.Children().ToList())
-            {
-                var childContent = ToKeyValue(child);
-                if (childContent != null)
-                {
-                    contentData = contentData.Concat(childContent)
-                       .ToDictionary(k => k.Key, v => v.Value);
-                }
-            }
-
-            return contentData;
-        }
-
-        var jValue = token as JValue;
-        if (jValue?.Value == null)
-        {
-            return null;
-        }
-
-        var value = jValue.Type == JTokenType.Date ? jValue.ToString("o", CultureInfo.InvariantCulture) : jValue.ToString(CultureInfo.InvariantCulture);
-
-        return new Dictionary<string, string> {{token.Path, value}};
-    }
-
     public static bool IsOrImplements(
         Type child,
         Type parentOrGivenObject)
