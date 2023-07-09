03dbc8b177653ce9dd85471e35ac0e0f9430de30
Michal Motyčka <mychalmotycka@gmail.com>
2023-07-09 14:08:59 +0200

    Renamed ThrowExceptionInsteadOfReturning500 to RethrowExceptionInsteadOfReturningHttpResponse. Renamed WasApplicationCalledFromTestClient to WasApplicationCreatedFromTest. Moved extensions to namespace Microsoft.AspNetCore.Builder

--- src/Ridge.AspNetCore/Setup/RidgeInstaller.cs
+++ src/Ridge.AspNetCore/Setup/RidgeInstaller.cs
@@ -3,7 +3,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ridge.AspNetCore.ExceptionHandling;
 
-namespace Ridge.AspNetCore.Setup;
+// namespace is correct
+namespace Microsoft.AspNetCore.Builder;
 
 /// <summary>
 ///     Ridge installer.
@@ -11,12 +12,12 @@ namespace Ridge.AspNetCore.Setup;
 public static class RidgeInstaller
 {
     /// <summary>
-    ///     Adds middleware which saves exceptions when application is called from test using ridge.
+    ///     Adds middleware which saves exceptions.
     ///     This middleware allows ridge to rethrow exceptions instead of returning 5xx code.
     /// </summary>
     /// <param name="applicationBuilder"></param>
     /// <returns></returns>
-    public static IApplicationBuilder ThrowExceptionInsteadOfReturning500(
+    public static IApplicationBuilder RethrowExceptionInsteadOfReturningHttpResponse(
         this IApplicationBuilder applicationBuilder)
     {
         return applicationBuilder.UseMiddleware<ExceptionSavingMiddleware>();
@@ -28,7 +29,7 @@ public static class RidgeInstaller
     /// </summary>
     /// <param name="app">Application</param>
     /// <returns>True if the app was created started from ridge client.</returns>
-    public static bool WasApplicationCalledFromTestClient(
+    public static bool WasApplicationCreatedFromTest(
         this IApplicationBuilder app)
     {
         var configuration = app.ApplicationServices.GetService<IConfiguration>();
